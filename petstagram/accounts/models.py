be8d917d6220cdc39fa4865d6625ebff4b70a0fd
MariaKVelcheva <maria.k.velcheva@gmail.com>
2026-02-19 19:50:44 +0100

    added: created a custom user model

--- petstagram/accounts/models.py
+++ petstagram/accounts/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth import models as auth_models, get_user_model
 from django.db import models
 
-# Create your models here.
+from petstagram.accounts.managers import AppUserManager
+
+
+class AppUser(auth_models.AbstractUser, auth_models.PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    objects = AppUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+UserModel = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to=UserModel,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    first_name = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    last_name = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    profile_picture = models.URLField(
+        blank=True,
+        null=True,
+    )
+
--- petstagram/settings.py
+++ petstagram/settings.py
@@ -104,3 +104,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.AppUser"
