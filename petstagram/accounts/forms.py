3d9268f0ce9207495c3e61ab0d5e9d5df80eb3f7
MariaKVelcheva <maria.k.velcheva@gmail.com>
2026-02-19 20:16:45 +0100

    added: register form, view, and template + login form and view

--- petstagram/accounts/forms.py
+++ petstagram/accounts/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AuthenticationForm
 
 from petstagram.accounts.models import UserModel
 
@@ -11,5 +12,33 @@ class AppUserChangeForm(UserChangeForm):
 class AppUserCreationForm(UserCreationForm):
     class Meta:
         model = UserModel
+        fields = ("email", )
+
+        widgets = {
+            "email": forms.EmailInput(
+                attrs={'placeholder': 'Email'}
+            ),
+
+            "password1": forms.PasswordInput(
+                attrs={'placeholder': 'Password'}
+            ),
+
+            "password2": forms.PasswordInput(
+                attrs={"placeholder": "Repeat password"}
+            ),
+        }
 
 
+class AppUserLoginForm(AuthenticationForm):
+    emails = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={"autofocus": True}),
+    )
+
+    password = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={"autocomplete": "current_password"},
+        )
+    )
--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,8 +1,23 @@
-from django.views.generic import TemplateView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView, DetailView
+
+from petstagram.accounts.forms import AppUserCreationForm, AppUserLoginForm
+from petstagram.accounts.models import Profile
+
+UserModel = get_user_model()
 
 
-class LoginPageView(TemplateView):
+class AppUserLoginView(LoginView):
+    form_class = AppUserLoginForm
     template_name = 'accounts/login-page.html'
+
+    def form_valid(self, form):
+        super().form_valid(form)
+        profile_instance, _ = Profile.objects.get_or_create(user=self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class ProfileDeleteView(TemplateView):
@@ -17,5 +32,9 @@ class ProfileEditPageView(TemplateView):
     template_name = 'accounts/profile-edit-page.html'
 
 
-class RegisterPageView(TemplateView):
+class AppUserRegisterView(CreateView):
+    model = UserModel
+    form_class = AppUserCreationForm
     template_name = 'accounts/register-page.html'
+    success_url = reverse_lazy('login')
+
