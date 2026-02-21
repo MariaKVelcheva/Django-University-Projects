7325c937c638afde0701d43bcfbe952a1691c6a1
MariaKVelcheva <maria.k.velcheva@gmail.com>
2026-02-21 22:33:28 +0100

    fixed: moved the accounts models in a separate directory for readability

--- petstagram/accounts/models/app_profile.py
+++ petstagram/accounts/models/app_profile.py
@@ -2,31 +2,7 @@ from django.contrib.auth import models as auth_models, get_user_model
 from django.db import models
 
 from petstagram.accounts.managers import AppUserManager
-
-
-class AppUser(auth_models.AbstractUser, auth_models.PermissionsMixin):
-    email = models.EmailField(
-        unique=True,
-    )
-
-    is_active = models.BooleanField(
-        default=True,
-    )
-
-    is_staff = models.BooleanField(
-        default=False,
-    )
-
-    objects = AppUserManager()
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
-    def __str__(self):
-        return self.email
-
-
-UserModel = get_user_model()
+from petstagram.accounts.models import UserModel
 
 
 class Profile(models.Model):
